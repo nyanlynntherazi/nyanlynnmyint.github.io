d05b9f65d2c8af9d643ee022423410b243b10823
Nyan Lynn Myint <nyanlynn.razi@gmail.com>
2020-04-06 10:49:09 +0630

    summary json

---
+++ data/Summary.js
@@ -0,0 +1,102 @@
+const summaryData = [
+    {
+        "Date": "27-03-2020",
+        "HosPt": 372,
+        "PUI": 380,
+        "Suspected": 2,
+        "Lab_Neg": 334,
+        "Confirmed": 8,
+        "Death": 0,
+        "Recovered": 0,
+        "Pending": 30
+    },
+    {
+        "Date": "28-03-2020",
+        "HosPt": 430,
+        "PUI": 428,
+        "Suspected": 2,
+        "Lab_Neg": 367,
+        "Confirmed": 8,
+        "Death": 0,
+        "Recovered": 0,
+        "Pending": 55
+    },
+    {
+        "Date": "29-03-2020",
+        "HosPt": 476,
+        "PUI": 474,
+        "Suspected": 2,
+        "Lab_Neg": 403,
+        "Confirmed": 10,
+        "Death": 0,
+        "Recovered": 0,
+        "Pending": 63
+    },
+    {
+        "Date": "30-03-2020",
+        "HosPt": 517,
+        "PUI": 515,
+        "Suspected": 2,
+        "Lab_Neg": 426,
+        "Confirmed": 14,
+        "Death": 0,
+        "Recovered": 0,
+        "Pending": 77
+    },
+    {
+        "Date": "31-03-2020",
+        "HosPt": 561,
+        "PUI": 559,
+        "Suspected": 2,
+        "Lab_Neg": 497,
+        "Confirmed": 15,
+        "Death": 1,
+        "Recovered": 0,
+        "Pending": 49
+    },
+    {
+        "Date": "01-04-2020",
+        "HosPt": 597,
+        "PUI": 595,
+        "Suspected": 2,
+        "Lab_Neg": 498,
+        "Confirmed": 16,
+        "Death": 1,
+        "Recovered": 0,
+        "Pending": 83
+    },
+    {
+        "Date": "02-04-2020",
+        "HosPt": 644,
+        "PUI": 642,
+        "Suspected": 2,
+        "Lab_Neg": 534,
+        "Confirmed": 20,
+        "Death": 1,
+        "Recovered": 0,
+        "Pending": 90
+    },
+    {
+        "Date": "03-04-2020",
+        "HosPt": 690,
+        "PUI": 688,
+        "Suspected": 2,
+        "Lab_Neg": 616,
+        "Confirmed": 20,
+        "Death": 1,
+        "Recovered": 0,
+        "Pending": 54
+    },
+    {
+        "Date": "04-04-2020",
+        "HosPt": 748,
+        "PUI": 746,
+        "Suspected": 2,
+        "Lab_Neg": 616,
+        "Confirmed": 21,
+        "Death": 1,
+        "Recovered": 0,
+        "Pending": 111
+    }
+];
+export default summaryData
